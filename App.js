76148af2e3c324f2b8c7836150943f5e66cfd1f2
hairenjun <hairenjun@hotmail.com>
2023-12-23 07:15:21 -0800

    can be build

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import WebView from 'react-native-webview';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+    source={{uri:"https://hairenjun.link/"}}
+    
+    style={{flex:1}}
+    onError={(syntheticEvent) => {
+      const { nativeEvent } = syntheticEvent;
+      console.warn('WebView error: ', nativeEvent);
+      alert("要么是没网，要么是用的EEI")
+      return TimeoutPage
+    }}/>
+
   );
 }
-
+const TimeoutPage = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Timeout Error: Could not load page.</Text>
+    </View>)}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
